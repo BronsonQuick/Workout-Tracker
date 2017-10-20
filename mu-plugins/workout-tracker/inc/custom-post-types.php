54b4807298a9af0823e37677f6856fac0c3a3e13
Bronson Quick <bronson@bronsonquick.com.au>
2017-10-20 16:02:52 +0200

    Add workouts

--- mu-plugins/workout-tracker/inc/custom-post-types.php
+++ mu-plugins/workout-tracker/inc/custom-post-types.php
@@ -3,8 +3,13 @@ namespace Workout_Tracker\CPT;
 
 function bootstrap() {
 	add_action( 'init', __NAMESPACE__ . '\\circuit_cpt' );
+	add_action( 'init', __NAMESPACE__ . '\\workout_cpt' );
 }
 
 function circuit_cpt() {
 	register_extended_post_type( 'circuit' );
+}
+
+function workout_cpt() {
+	register_extended_post_type( 'workout' );
 }
