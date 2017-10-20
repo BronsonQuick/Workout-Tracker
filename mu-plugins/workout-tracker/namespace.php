247e82adc3570100cbc2321239a057cd3fb14780
Bronson Quick <bronson@bronsonquick.com.au>
2017-10-20 16:17:28 +0200

    Start getting ready for the meta box functions

--- mu-plugins/workout-tracker/namespace.php
+++ mu-plugins/workout-tracker/namespace.php
@@ -9,6 +9,9 @@ Author URI: https://www.bronsonquick.com.au/
  */
 
 namespace Workout_Tracker;
+
 require_once( __DIR__ . '/inc/custom-post-types.php' );
+require_once( __DIR__ . '/inc/custom-meta-boxes.php' );
 
 add_action( 'muplugins_loaded', __NAMESPACE__ . '\\CPT\\bootstrap' );
+add_action( 'muplugins_loaded', __NAMESPACE__ . '\\CMB\\bootstrap' );
